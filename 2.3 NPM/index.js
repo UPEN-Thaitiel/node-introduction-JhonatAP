82e613e413170236e99608074ae59f7a9a972d21
JhonatAP <jhonnypolanco.jp@gmail.com>
2025-03-03 01:47:06 -0700

    Commit tarea Node

--- 2.3 NPM/index.js
+++ 2.3 NPM/index.js
@@ -1,9 +1,13 @@
 //Initialize NPM with npm init command
-
 // Install sillyname package with npm install
-
 // Use sillyname package to print a random silly name
+import generateName from "sillyname";
+import { randomSuperhero } from "superheroes";
 
+var sillyName = generateName();
+console.log(sillyName);
 // Install superheroes package with npm install
-
 // Use superheroes package to print a random silly name
+
+var superheroeName = randomSuperhero();
+console.log(superheroeName);
